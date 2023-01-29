ad74a9c0e6baed4cdf8f6fe9d315f8cb11c6195c
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-01-30 00:47:16 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,95 +6,63 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
 export function App() {
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    )
+  })
+      const [filter, setFilter] = useState('');
 
-    const [contacts, setContacts] = useState([
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },]);
-    const [filter, setFilter] = useState('');
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
+  const addContact = ({ name, number }) => {
+    const contact = {
+      name,
+      number,
+      id: nanoid(),
+    };
 
-useEffect(() => {
-
-  
-})
-
-
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      return alert(`${name} is already in contacts!`);
     }
 
+    return setContacts([contact, ...contacts]);
+  };
 
-//   componentDidMount() {
-//     console.log('App componentDidMount');
+  const deleteContact = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
+  };
 
-//     const contacts = localStorage.getItem('contacts');
-//     const parsedContacts = JSON.parse(contacts);
+  const changeFilter = event => {
+    setFilter(event.currentTarget.value);
+  };
 
-//     if (parsedContacts) {
-//       this.setState({ contacts: parsedContacts });
-//     }
-//   }
+  const searchContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
 
-//   componentDidUpdate(prevProps, prevState) {
-//     console.log('Add componentDidUpdate');
-
-//     if (this.state.contacts !== prevState.contacts) {
-//       console.log('Oбновилось поле contacts, записываю contacts в хранилище');
-//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//     }
-//   }
-
-//   addContact = ({ name, number }) => {
-//     const contact = {
-//       name,
-//       number,
-//       id: nanoid(),
-//     };
-
-//     this.setState(({ contacts }) => {
-//       if (
-//         contacts.find(
-//           contact => contact.name.toLowerCase() === name.toLowerCase()
-//         )
-//       ) {
-//         return alert(`${name} is already in contacts!`);
-//       }
-
-//       return {
-//         contacts: [contact, ...contacts],
-//       };
-//     });
-//   };
-
-//   deleteContact = contactId => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-//     }));
-//   };
-
-//   changeFilter = event => {
-//     this.setState({ filter: event.currentTarget.value });
-//   };
-
-//   searchContacts = () => {
-//     const { filter, contacts } = this.state;
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(filter.toLowerCase())
-//     );
-//   };
-
-//   render() {
-//     return (
-//       <section className={css.section}>
-//         <h1>Phonebook</h1>
-//         <ContactForm onSubmit={this.addContact} />
-//         <h2>Contacts</h2>
-//         <Filter filter={this.state.filter} changeFilter={this.changeFilter} />
-//         <ContactList
-//           contacts={this.searchContacts()}
-//           deleteContact={this.deleteContact}
-//         />
-//       </section>
-//     );
-//   }
-// }
+  return (
+    <section className={css.section}>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} changeFilter={changeFilter} />
+      <ContactList contacts={searchContacts()} deleteContact={deleteContact} />
+    </section>
+  );
+}
